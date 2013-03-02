1e757a09ea24b6e351360d552d7aca2dbff4cb70
Jordan Scales <scalesjordan@gmail.com>
2013-03-02 13:07:13 -0500

    refactored obstacles into patterns

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ game = (function() {
     lane: 0,
     muted: false,
     panes: [],
+    clock: 0,
 
     start: function() {
       var that = this;
@@ -29,7 +30,7 @@ game = (function() {
       for (i in this.panes) {
         var pane = this.panes[i];
 
-        if (!pane.broken && pane.lane == game.lane && pane.y > 485 && pane.y < 515) {
+        if (!pane.broken && pane.lane == game.lane && pane.y > 485 && pane.y < 500) {
           pane.broken = true;
           audio.glass();
 
@@ -46,9 +47,44 @@ game = (function() {
       gfx.hero(400 + 150*this.lane - 28, 500, this.state);
 
       // add a pane of glass
-      if (Math.random() < 0.05) {
-        var lane = Math.floor(Math.random() * 3) - 1;
-        this.panes.push({ lane: lane, y: -20, broken: false });
+      // if (Math.random() < 0.05) {
+      //   var lane = Math.floor(Math.random() * 3) - 1;
+      //   this.panes.push({ lane: lane, y: -20, broken: false });
+      // }
+      if ((this.clock % 200) == 0)
+        this.add_pattern();
+
+      this.clock++;
+
+    },
+
+    add_pattern: function() {
+      var lpane = { type: 'pane', lane: -1 },
+          mpane = { type: 'pane', lane: 0 },
+          rpane = { type: 'pane', lane: 1 },
+          i, j, y = -20,
+          choice,
+          to_add,
+          patterns;
+
+      patterns = [
+        [],
+        [],
+        [],
+        [lpane, lpane, lpane, lpane, lpane],
+        [mpane, mpane, mpane, mpane, mpane],
+        [rpane, rpane, rpane, rpane, rpane],
+        [lpane, lpane, mpane, mpane, rpane, rpane],
+        [rpane, rpane, mpane, mpane, lpane, lpane]
+      ];
+
+      to_add = Math.floor(Math.random() * 5) + 1;
+      for (i = 0; i < to_add; i++) {
+        choice = patterns[Math.floor(Math.random() * patterns.length)];
+        for (j in choice) {
+          this.panes.push({ broken: false, lane: choice[j].lane, y: y });
+          y -= 60;
+        }
       }
 
     }
